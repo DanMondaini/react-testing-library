cc2873fc38dfe05a906993822455d27afe15f89b
DanMondaini <danielmondaini@outlook.com>
2021-11-08 15:22:17 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Exibe mensagem quando não há pokemons favoritos', () => {
+    render(<FavoritePokemons />);
+    const pokemonNotFoundMessage = screen.getByText('No favorite pokemon found');
+    expect(pokemonNotFoundMessage).toBeInTheDocument();
+  });
+});
